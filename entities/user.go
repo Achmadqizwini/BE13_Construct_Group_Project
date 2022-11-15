e25bcb86607c9036f89a848d534a83e2f3472965
Achmad Qizwini <akise.shun@gmail.com>
2022-11-15 10:16:00 +0700

    add register & edit gender

--- entities/user.go
+++ entities/user.go
@@ -1,9 +1,9 @@
 package entities
 
 type User struct {
-	Id   int
-	Nama string
-	// Gender
+	Id         int
+	Nama       string
+	Gender     int
 	No_telepon int
 	Password   string
 	Saldo      int
