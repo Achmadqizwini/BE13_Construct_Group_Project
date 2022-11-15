6f73fadda5b47c78f49875bb02b31e645a205a98
unknown <techmin7@gmail.com>
2022-11-15 14:30:45 +0700

    update register main, entities, controllers

--- controllers/user.go
+++ controllers/user.go
@@ -1,1 +1,21 @@
 package controllers
+
+func AccountRegister ()  {
+var query = "Insert into user(Nama, Gender, No_telepon, Password) Values (?, ?, ?, ?)"
+			statement, errPrepare := db.Prepare(query)
+			if errPrepare != nil {
+				log.Fatal("error prepare Register ", errPrepare.Error())
+			}
+
+			result, errExec := statement.Exec(newUser.Nama, newUser.Gender, newUser.No_telepon, newUser.Password)
+			if errExec != nil {
+				log.Fatal("error exec Register", errExec.Error())
+			} else {
+				row, _ := result.RowsAffected()
+				if row > 0 {
+					fmt.Println("Register berhasil")
+				} else {
+					fmt.Println("Register gagal")
+				}
+			}
+		)
