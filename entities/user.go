ef9387a2c0b4e6baaf2c535522dfefad7a7a8b1d
Achmad Qizwini <akise.shun@gmail.com>
2022-11-17 08:11:03 +0700

    update entities user

--- entities/user.go
+++ entities/user.go
@@ -1,11 +1,13 @@
 package entities
 
+import "time"
+
 type User struct {
 	Id         int
 	Nama       string
-	Gender     int
+	Gender     string
 	No_telepon string
 	Password   string
 	Saldo      int
-	// 	Created_at time.Now()
+	Created_at time.Time
 }
