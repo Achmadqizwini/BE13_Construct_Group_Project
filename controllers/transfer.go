661de8663ddb068111c6a8b0515e094f41952e2f
Achmad Qizwini <akise.shun@gmail.com>
2022-11-18 01:08:14 +0700

    update controllers for transaction

--- controllers/transfer.go
+++ controllers/transfer.go
@@ -7,51 +7,19 @@ import (
 	"time"
 )
 
-// func Transfer(db *sql.DB, transfer entities.User, id_account int, saldo_user int) (int, error) {
-// 	var nominal int
-// 	var detail entities.Transfer
-// 	var query = "Insert into Transfer (User_id, User_id_tujuan, nominal, keterangan) Values (?, ?, ?,?,?)"
-// 	statement, errPrepare := db.Prepare(query)
-// 	if errPrepare != nil {
-// 		log.Fatal("error prepare top up", errPrepare.Error())
-// 	}
-// 	idTujuan := db.QueryRow("select id from users where no_telepon in(?)", transfer.No_telepon)
-
-// 	var userrow entities.User
-// 	errScan := idTujuan.Scan(&userrow.Id)
-// 	if errScan != nil {
-// 		log.Fatal("error scan no telpon ", errScan.Error())
-// 	}
-// 	if saldo_user > nominal {
-// 		result, errExec := statement.Exec(id_account, userrow.Id, nominal, detail.Keterangan, detail.Created_at)
-// 		if errExec != nil {
-// 			return -1, errExec
-// 		} else {
-// 			row, errRow := result.RowsAffected()
-// 			if row > 0 {
-// 				return int(row), nil
-// 			} else {
-// 				return -1, errRow
-// 			}
-// 		}
-// 	} else {
-// 		return -1, sql.ErrNoRows
-// 	}
-// }
-
-func Transfer(db *sql.DB, entity entities.User, entity2 entities.Transfer, id_account int, nominal *int) (int, error) {
+func Transfer(db *sql.DB, entity entities.User, entity2 entities.Transfer, id_account int, nominal int) (int, error) {
 	entity2.Created_at = time.Now()
 	var query = "Insert into Transfer (User_id_Pengirim, User_id_Penerima, nominal, keterangan, created_at) Values (?, ?, ?,?,?)"
 	statement, errPrepare := db.Prepare(query)
 	if errPrepare != nil {
-		log.Fatal("error prepare top up", errPrepare.Error())
+		return -1, errPrepare
 	}
-	idTujuan := db.QueryRow("select id from users where no_telepon in(?)", entity.No_telepon)
 
+	idTujuan := db.QueryRow("select id from users where no_telepon in(?)", entity.No_telepon)
 	var userrow entities.User
 	errScan := idTujuan.Scan(&userrow.Id)
 	if errScan != nil {
-		log.Fatal("error scan no telpon ", errScan.Error())
+		return -1, errScan
 	}
 
 	result, errExec := statement.Exec(id_account, userrow.Id, nominal, entity2.Keterangan, entity2.Created_at)
@@ -64,5 +32,43 @@ func Transfer(db *sql.DB, entity entities.User, entity2 entities.Transfer, id_ac
 		} else {
 			return int(row), nil
 		}
+	}
+}
+
+func Transfer_history(db *sql.DB, id_account int) ([]entities.History_Transfer_Respon, error) {
+	result, errSelect := db.Query("select u.nama, v.nama, transfer.nominal, transfer.keterangan, transfer.created_at from transfer inner join users u on transfer.user_id_pengirim = u.id inner join users v on transfer.user_id_penerima = v.id where transfer.user_id_pengirim = (?);", id_account)
+	if errSelect != nil {
+		log.Fatal("error select", errSelect.Error())
+	}
+
+	var dataHistory []entities.History_Transfer_Respon
+	for result.Next() {
+		var userrow entities.History_Transfer_Respon
+		errScan := result.Scan(&userrow.Nama_Pengirim, &userrow.Nama_Penerima, &userrow.Nominal, &userrow.Keterangan, &userrow.Created_at)
+		if errScan != nil {
+			log.Fatal("error scan", errScan.Error())
+		}
+		dataHistory = append(dataHistory, userrow)
+	}
+	return dataHistory, nil
+}
+
+func TambahSaldo_tf(db *sql.DB, nominal int, saldo int, ent entities.User) (int, error) {
+	var query = ("Update users set saldo = ? where no_telepon = ?")
+	statement, errPrepare := db.Prepare(query)
+	if errPrepare != nil {
+		return -1, errPrepare
+	}
+
+	var newsaldo = saldo + nominal
+	result, errExec := statement.Exec(newsaldo, ent.No_telepon)
+	if errExec != nil {
+		return -1, errExec
+	} else {
+		_, errRow := result.RowsAffected()
+		if errRow != nil {
+			return 0, errRow
+		}
+		return newsaldo, nil
 	}
 }
